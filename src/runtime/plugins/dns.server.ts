3a4fa3e23d12174ce9b82038012fd1a614dce379
Benjamin Canac <canacb1@gmail.com>
2024-11-28 10:43:29 +0100

    fix(plugins): infer type from `#app`

--- src/runtime/plugins/dns.server.ts
+++ src/runtime/plugins/dns.server.ts
@@ -1,4 +1,6 @@
 import { defineNuxtPlugin } from '#imports'
+// FIXME: https://github.com/nuxt/module-builder/issues/141#issuecomment-2078248248
+import type {} from '#app'
 
 // Fixes `ECONNREFUSED` on Node 18: https://github.com/node-fetch/node-fetch/issues/1624#issuecomment-1407717012
 export default defineNuxtPlugin({
--- src/runtime/plugins/strapi.ts
+++ src/runtime/plugins/strapi.ts
@@ -1,6 +1,8 @@
 import { useStrapiAuth } from '../composables/useStrapiAuth'
 import { useStrapiUser } from '../composables/useStrapiUser'
 import { defineNuxtPlugin } from '#imports'
+// FIXME: https://github.com/nuxt/module-builder/issues/141#issuecomment-2078248248
+import type {} from '#app'
 
 export default defineNuxtPlugin(async () => {
   const user = useStrapiUser()
